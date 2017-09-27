78405187ff6a67cc531bfa40396fbef920e85bbf
kashkama <owen.kamau2014@gmail.com>
2017-09-27 11:53:25 +0300

    adding react-bootstrap dependency

--- webpack.config.js
+++ webpack.config.js
@@ -59,7 +59,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template:'template.ejs',
       appMountId: 'root',
-      title: 'Queue',
+      title: 'React App',
       filename: resolve(__dirname, "dist", "index.html"),
     }),
     new CleanWebpackPlugin(['dist'])
