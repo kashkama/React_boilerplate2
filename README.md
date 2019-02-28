38275133182e0494478aa71c572095c7389258d5
kashkama <owen.kamau2014@gmail.com>
2019-02-28 12:57:31 +0300

    upgrading dependencies using the right dependencies in webpack. using yarn rearranging the source directory

---
+++ README.md
@@ -0,0 +1,6 @@
+# React_BoilerPlate2
+
+* A simple and fast starter kit tool for ReactJS. Just `npm install` then `npm run start`.
+
+## Note
+* Dependencies may be outdated. Update yourself where necessary.
---
+++ webpack.config.js
@@ -0,0 +1,81 @@
+const webpack = require("webpack");
+const {resolve} = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+
+module.exports = {
+
+    entry: [
+        "babel-polyfill",
+        "react-hot-loader/patch",
+        "webpack-dev-server/client?http://localhost:8080",
+        "webpack/hot/only-dev-server",
+        resolve(__dirname, "src", "index.jsx")
+    ],
+
+    output: {
+        filename: "app.bundle.js",
+        path: resolve(__dirname, "dist"),
+        publicPath: "/"
+    },
+
+    resolve: {
+        extensions: [".js", ".jsx"]
+    },
+
+    devtool: "#source-map",
+
+    devServer: {
+        hot: true,
+        contentBase: resolve(__dirname, "dist"),
+        stats: "errors-only",
+        open: true,
+        publicPath: "/"
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                enforce: "pre",
+                loader: "eslint-loader",
+                exclude: /node_modules/,
+                options: {
+                    emitWarning: true,
+                    configFile: "./.eslintrc.json"
+                }
+            },
+            {
+                test: /\.jsx?$/,
+                loader: "babel-loader",
+                exclude: /node_modules/,
+                options: {
+                    presets: ["@babel/preset-env"],
+                    plugins: ["react-hot-loader/babel", "@babel/plugin-transform-runtime"]
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"]
+            },
+            {
+                test:/\.(jpe?g|png|gif|svg)$/i,
+                use: [
+                    "url-loader?limit=10000"
+                ]
+            }
+        ]
+    },
+
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NamedModulesPlugin(),
+        new HtmlWebpackPlugin({
+            template: "template.ejs",
+            appMountId: "root",
+            title: "React_App",
+            filename: resolve(__dirname, "dist", "index.html")
+        }),
+        new CleanWebpackPlugin(["dist"])
+    ]
+};
